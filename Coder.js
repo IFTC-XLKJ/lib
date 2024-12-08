7a7c99aab7ce3f9bba84e6fd8b3f6df87c041ae3
IFTC-XLKJ <iftcceo@139.com>
2024-12-08 16:35:11 +0800

    Update Coder.js

--- Coder.js
+++ Coder.js
@@ -1,12 +1,17 @@
 class Coder {
     constructor(code) {
-        this.#init(code);
+        this.get(this.#init(code));
     }
     #init(code) {
         let result = code
         // HTML highlight
         const html = new DOMParser().parseFromString(result, 'text/html')
-        console.log(html)
+        const nodes = html.querySelectorAll("*")
+        console.log(nodes)
         return result
+    }
+
+    get(code) {
+        return code
     }
 }
