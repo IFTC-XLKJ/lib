516986dcdd3aa55378015662e8c6346b577fccc4
IFTC-XLKJ <iftcceo@139.com>
2024-12-08 12:45:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ onload = e => {
             const lib = libs.filter(lib => lib.toLowerCase().includes(search.value.toLowerCase()))
             if (lib) {
                 console.log(lib)
+                result.innerHTML = '搜索结果:'
+                for (let i = 0; i < lib.length; i++) {
+                    result.innerHTML += `<li><a href="https://www.npmjs.com/package/${lib[i]}">${lib[i]}</a></li>`
+                }
             }
         }
     }
