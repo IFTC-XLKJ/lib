38ed0657f1f538be7b81e3660067157be5f9dfd8
IFTC-XLKJ <iftcceo@139.com>
2024-12-08 12:37:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-onload = e => { }
+window.libs = []
+onload = e => {
+    const libsLI = document.querySelectorAll('ol>li');
+    console.log(libsLI)
+    for (let i = 0; i < libsLI.length; i++) {
+        const lib = libsLI[i].innerText.trim()
+        libs.push(lib)
+    }
+}
